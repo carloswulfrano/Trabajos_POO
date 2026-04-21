d5eab11aa2e5b0bedd876c4799b2966e17f4b6f1
carloswulfrano <carlosdanielwulfranocruz@gmail.com>
2026-04-21 13:54:31 -0600

    Agregando el xml (maven) en mi projecto

---
+++ src/FesAragon_POO/Trabajos_Actividades_Clase/Abril/Clase_21_04_2026_IMC/CalculadoraIMC.java
@@ -0,0 +1,11 @@
+package FesAragon_POO.Trabajos_Actividades_Clase.Abril.Clase_21_04_2026_IMC;
+
+
+public class CalculadoraIMC {
+
+    private double peso;
+    private double altura;
+    private double imc;
+
+
+}
